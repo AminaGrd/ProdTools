2a68723cac014a700f215fe6379f39c98abcca6d
AminaGrd <amina.giraud@gmail.com>
2022-09-12 17:18:53 +0200

    modification on show/unshow js

--- prodtools.js
+++ prodtools.js
@@ -1,20 +1,45 @@
+const generated = document.getElementsByClassName('not-generated')[0];
+const generated1 = document.getElementsByClassName('not-generated')[1];
+const generated2 = document.getElementsByClassName('not-generated')[2];
+const unshow = document.getElementsByClassName('unshow')[0];
+const unshow1 = document.getElementsByClassName('unshow')[1];
+const unshow2 = document.getElementsByClassName('unshow')[2];
+// let i = 0;
+
+
 document.getElementById('generate').addEventListener('click', () => {
+
     const nameA = document.getElementById('name-archive').value;
     const namefileA = document.getElementById('name-file-archive').value;
     const namefileExt = document.getElementById('name-file-ext').value;
-    const generated = document.getElementsByClassName('not-generated');
-    let i = 0;
+    const inputScp = document.getElementById("scp");
 
-    // console.log(`scp -r ${namefileA}instaler.php espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt} && scp -r ${namefileA}${nameA} espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt}`);
+    console.log(`scp -r ${namefileA}instaler.php espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt} && scp -r ${namefileA}${nameA} espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt}`);
 
-    // if(getComputedStyle(generated).display != "none"){
-    //     generated.style.display = "none";
-    //   } else {
-    //     generated.style.display = "block";
-    //   }
-    generated.classList.toggle('not-generated--show');
+    const SCP = `scp -r ${namefileA}instaler.php espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt} && scp -r ${namefileA}${nameA} espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt}`
+
+    generated.style.display = 'block';
+    generated1.style.display = 'block';
+    generated2.style.display = 'block';
+    unshow.style.display = 'none';
+    unshow1.style.display = 'none';
+    unshow2.style.display = 'none';
+
+    // document.inputScp.value = SCP;
+    // alert(inputScp);
+
+    // if (i >= 2) {
+    //     navbarlanding.classList.toggle('not-generated--unshow');
+    // }
 
 });
+
+const resetButton = document.getElementbyId('return');
+resetButton.onclick= reloadPage;
+
+function reloadPage(){
+    window.location.reload();
+}
 
 
 // TEST
