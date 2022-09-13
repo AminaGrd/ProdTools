73614a43a5c4ce87575b30fa6bef04c51d5b491b
AminaGrd <amina.giraud@gmail.com>
2022-09-13 12:00:01 +0200

    finish js html function good

--- prodtools.js
+++ prodtools.js
@@ -4,7 +4,6 @@ const generated2 = document.getElementsByClassName('not-generated')[2];
 const unshow = document.getElementsByClassName('unshow')[0];
 const unshow1 = document.getElementsByClassName('unshow')[1];
 const unshow2 = document.getElementsByClassName('unshow')[2];
-// let i = 0;
 
 
 document.getElementById('generate').addEventListener('click', () => {
@@ -12,7 +11,6 @@ document.getElementById('generate').addEventListener('click', () => {
     const nameA = document.getElementById('name-archive').value;
     const namefileA = document.getElementById('name-file-archive').value;
     const namefileExt = document.getElementById('name-file-ext').value;
-    const inputScp = document.getElementById("scp");
 
     console.log(`scp -r ${namefileA}instaler.php espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt} && scp -r ${namefileA}${nameA} espripk-preprod@ssh.cluster030.hosting.ovh.net:${namefileExt}`);
 
@@ -25,12 +23,7 @@ document.getElementById('generate').addEventListener('click', () => {
     unshow1.style.display = 'none';
     unshow2.style.display = 'none';
 
-    // document.inputScp.value = SCP;
-    // alert(inputScp);
-
-    // if (i >= 2) {
-    //     navbarlanding.classList.toggle('not-generated--unshow');
-    // }
+    document.getElementById('scp').value = SCP
 
 });
 
@@ -41,19 +34,20 @@ function reloadPage(){
     window.location.reload();
 }
 
+function copyToClipBoardArchive() {
 
-// TEST
+    const content = document.getElementById('name-archive').value;
+    
+    navigator.clipboard.writeText(content)
 
-// document.getElementById('test').addEventListener("click", () => {
-//     const generated = document.getElementById('generated');
-//     const test = getComputedStyle(generated).display
-//     console.log(test);
-    // if(getComputedStyle(generated).display != "none"){
-    //   generated.style.display = "none";
-    // } else {
-    //   generated.style.display = "block";
-    // }
-//   })
+}
+function copyToClipBoardSCP() {
+
+    const content = document.getElementById('scp').value;
+    
+    navigator.clipboard.writeText(content)
+
+}
 
 
 
